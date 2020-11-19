fb23b50642460530b03a8a0fea1bf26fa7273e0e
syrix78 <lucas.hornung@hotmail.com>
2020-11-19 15:46:24 -0500

    Added grammar reduction

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from nltk import Production
 from nltk.parse import ViterbiParser
 
 train = treebank.fileids()[:190]
-test = treebank.fileids()[195:]
+#test = treebank.fileids()[190:]
+#All tests samples ordered by the number of words
+test = ["wsj_0191.mrg", "wsj_0195.mrg", "wsj_0196.mrg", "wsj_0199.mrg", "wsj_0193.mrg", "wsj_0197.mrg", "wsj_0198.mrg", "wsj_0194.mrg", "wsj_0192.mrg",]
 
-#It is impossible to use all productions rules because the nltk VertibiParser is too slow
-
-def train_grammar(unknown_words=[]):
+def train_grammar(unknown_words=[], nb_reduced_production=6000):
 
     productions = []
 
@@ -30,24 +30,24 @@ def train_grammar(unknown_words=[]):
             productions += tree.productions()
 
 
+    counter = collections.Counter(productions)
+    n_comms = [item for item, count in counter.most_common(nb_reduced_production) for i in range(count)]
+
+    #Adding unkwown words and terminal rules back into the reduced productions set
     unknown_words_prods = []
-    for u in unknown_words:
-        rhs = [u]
-        for p in productions:
-            if isinstance(p._rhs[0], str):
+    for p in productions:
+        if isinstance(p._rhs[0], str):
+            unknown_words_prods.append(p)
+            for u in unknown_words:
+                rhs = [u]
                 lhs = p._lhs
                 new_prod = Production(lhs, rhs)
                 unknown_words_prods.append(new_prod)
-    productions += unknown_words_prods
-    #counter = collections.Counter(productions)
-    #if len(unknown_words) == 0:
-        #n_comms = [item for item, count in counter.most_common(100) for i in range(count)]
-    #else:
-    n_comms = productions
-    S = Nonterminal('S')
-    #print(productions)
-    grammar = induce_pcfg(S, n_comms)
 
+
+    n_comms += unknown_words_prods
+    S = Nonterminal('S')
+    grammar = induce_pcfg(S, n_comms)
 
     return grammar
 
@@ -57,22 +57,15 @@ def get_fixed_grammer(grammar, tokens):
     return train_grammar(missing)
 
 def test_sentences(grammar):
-    demos = ['I saw John with my telescope']
-    sent = demos[0]
 
     for t in test:
         print("Processing: " + str(t))
         reference = list(treebank.tagged_words(t))
 
-
         tokens = list(treebank.words(t))
 
-
-
-        # Tokenize the sentence.
-        #tokens = sent.split()
         print("fixing grammar.....")
-        # Checks if grammar covers all words in the sentence
+        # Checks if grammar covers all words in the sentence and adds them to the grammar if necessary
         fixed_grammar = get_fixed_grammer(grammar, tokens)
 
         print("fixed grammar")
@@ -85,8 +78,11 @@ def test_sentences(grammar):
         parses = parser.parse_all(tokens)
         print("Time")
         print(start - time.time())
+
+        #Getting POS tags from parser tree
         leafs = parses[0].pos()
 
+        #Calculating accuracy of Parser results
         correct_tags = 0.0
         for i in range(len(leafs)):
             if leafs[i] == reference[i]:
@@ -96,8 +92,6 @@ def test_sentences(grammar):
         print(str(correct_tags/len(leafs)))
 
 if __name__ == "__main__":
-
-
     grammar = train_grammar()
     test_sentences(grammar)
-    #print(grammar)
+
